8ce6c708b79fd45fdccf6f41e359285f28396fae
Amr Kaled <amrkhaled033@gmail.com>
2025-04-07 23:30:59 +0200

    adding some packages

--- lib/presentation/features/prayer_times/views/prayer_times.dart
+++ lib/presentation/features/prayer_times/views/prayer_times.dart
@@ -1,19 +1,59 @@
-import 'package:flutter/material.dart';
-import 'package:ma_fatani/presentation/widgets/custom_settings_screen_body.dart';
+import 'dart:convert';
 
-class PrayerTimesScreen extends StatelessWidget {
+import 'package:adhan_dart/adhan_dart.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:ma_fatani/main.dart';
+import 'package:ma_fatani/presentation/widgets/custom_settings_screen_body.dart';
+import 'package:http/http.dart' as http;
+
+class PrayerTimesScreen extends StatefulWidget {
   const PrayerTimesScreen({super.key});
 
   @override
+  State<PrayerTimesScreen> createState() => _PrayerTimesScreenState();
+}
+
+class _PrayerTimesScreenState extends State<PrayerTimesScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    getPrayerTimesFromApi();
+  }
+
+  Future<void> getPrayerTimesFromApi() async {
+    final url = Uri.parse(
+      'https://api.aladhan.com/v1/timings?latitude=${position!.latitude}&longitude=${position!.longitude}&method=4',
+    );
+
+    final res = await http.get(url);
+    if (res.statusCode == 200) {
+      final data = jsonDecode(res.body);
+      final timings = data['data']['timings'];
+      print("SS--Fajr: ${timings['Fajr']}");
+      print("SS--Dhuhr: ${timings['Dhuhr']}");
+      print("SS--Asr: ${timings['Asr']}");
+      print("SS--Maghrib: ${timings['Maghrib']}");
+      print("SS--Isha: ${timings['Isha']}");
+    } else {
+      print("Error fetching prayer times");
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const CustomSettingsScreenBody(
+    return CustomSettingsScreenBody(
       title: 'Prayer Times',
       child: Column(
+        spacing: 20,
         children: [
-          Text(
+          const Text(
             'Prayer time configurations...',
             style: TextStyle(fontSize: 16),
           ),
+
           // Add prayer time controls here
         ],
       ),
